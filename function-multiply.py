fcf178430568b5a566d2080425c3ff1f24ac474c
Ewa B <ewabielawska@daum.net>
2019-11-10 21:05:20 +0100

    new file

---
+++ function-multiply.py
@@ -0,0 +1,12 @@
+
+# JS to Python
+# function multiply(num1,num2) {
+#   var result = num1 * num2;
+#   return result;
+# }
+
+def multiply(num1,num2):
+    result = num1 * num2;
+    return result
+
+print(multiply(3,4))
